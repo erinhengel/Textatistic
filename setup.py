94de2ce7b8e119787b8f4ab17d1920613bcb5810
Erin Hengel <erin.hengel@gmail.com>
2015-10-21 16:15:15 +0200

    modified:   setup.py

--- setup.py
+++ setup.py
@@ -4,15 +4,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='textatistic',
-    version='0.1',
-    packages = find_packages(),
-    scripts = ['textatistic.py'],
+    version='0.001',
+    packages = ['textatistic'],
     install_requires = ['pyhyphen>=2.0.5'],
-    package_data = {'': ['dale_chall.txt', 'abbreviations.txt', 'LICENSE']}
-    description='Calculate readability scores (Flesch Reading Ease, etc.).',
-    long_description=open('README.md').read(),
+    package_data = {'': ['LICENSE'], 'textatistic': ['dale_chall.txt', 'abbreviations.txt']},
     author='Erin Hengel',
     author_email='erin.hengel@gmail.com',
+    description='Calculate readability scores (Flesch Reading Ease, etc.).',
+    long_description=open('README.md').read(),
     url='http://www.erinhengel.com/software/textatistic/',
     license='Apache 2.0',
     classifiers=(
